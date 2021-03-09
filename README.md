9a6f747b2e4f11a7a61eb8c3ccdf4c0e307f5149
Cybot101 <cybot101@gmail.com>
2021-03-09 13:02:54 +1100

    Initial files for MakeCode project

---
+++ README.md
@@ -0,0 +1,31 @@
+
+> Open this page at [https://cybot101.github.io/colour_reaction_demo_emc/](https://cybot101.github.io/colour_reaction_demo_emc/)
+
+## Use as Extension
+
+This repository can be added as an **extension** in MakeCode.
+
+* open [https://makecode.microbit.org/](https://makecode.microbit.org/)
+* click on **New Project**
+* click on **Extensions** under the gearwheel menu
+* search for **https://github.com/cybot101/colour_reaction_demo_emc** and import
+
+## Edit this project ![Build status badge](https://github.com/cybot101/colour_reaction_demo_emc/workflows/MakeCode/badge.svg)
+
+To edit this repository in MakeCode.
+
+* open [https://makecode.microbit.org/](https://makecode.microbit.org/)
+* click on **Import** then click on **Import URL**
+* paste **https://github.com/cybot101/colour_reaction_demo_emc** and click import
+
+## Blocks preview
+
+This image shows the blocks code from the last commit in master.
+This image may take a few minutes to refresh.
+
+![A rendered view of the blocks](https://github.com/cybot101/colour_reaction_demo_emc/raw/master/.github/makecode/blocks.png)
+
+#### Metadata (used for search, rendering)
+
+* for PXT/microbit
+<script src="https://makecode.com/gh-pages-embed.js"></script><script>makeCodeRender("{{ site.makecode.home_url }}", "{{ site.github.owner_name }}/{{ site.github.repository_name }}");</script>
---
+++ main.ts
@@ -0,0 +1,69 @@
+let winner = 0
+let strip = neopixel.create(DigitalPin.P0, 8, NeoPixelMode.RGB)
+let state = "idle"
+let led_p1 = strip.range(4, 8)
+let led_p2 = strip.range(0, 3)
+basic.forever(function () {
+    let colour = 0;
+    switch (randint(0, 2))
+    {
+        case 0:
+            colour = neopixel.colors(NeoPixelColors.Red)
+            break;
+        case 1:
+            colour = neopixel.colors(NeoPixelColors.Green)
+            break;
+        case 2:
+            colour = neopixel.colors(NeoPixelColors.Blue)
+            break;
+        default:
+            colour = neopixel.colors(NeoPixelColors.Yellow)
+            break;
+    }
+
+    strip.showColor(neopixel.colors(NeoPixelColors.White))
+    pause(randint(1000, 4000))
+    strip.showColor(colour)
+    winner = 0
+
+    for (let index = 0; index < 3000 && winner == 0; index++) {
+        pause(1)
+    }
+
+    strip.showColor(neopixel.colors(NeoPixelColors.Black))
+    if (winner == 1) {
+        for (let i = 0; i<5; i++) {
+            led_p1.showColor(colour)
+            pause(300)
+            led_p1.showColor(neopixel.colors(NeoPixelColors.Black))
+            pause(300)
+        }
+    } else if (winner == 2) {
+    	for (let i = 0; i<5; i++) {
+            led_p2.showColor(colour)
+            pause(300)
+            led_p2.showColor(neopixel.colors(NeoPixelColors.Black))
+            pause(300)
+        }
+    } else {
+    	
+    }
+
+    strip.showRainbow()
+    for (let i=0; i<200; i++) {
+        strip.rotate()
+        strip.show()
+        pause(50)
+    }
+})
+
+input.onPinPressed(TouchPin.P1, function () {
+    if (winner == 0) {
+        winner = 1;
+    }
+})
+input.onPinPressed(TouchPin.P2, function () {
+    if (winner == 0) {
+        winner = 2;
+    }
+})
